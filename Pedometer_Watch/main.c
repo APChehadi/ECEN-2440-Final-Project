26fa9b9031ef37333b9b93fbc2d6f783bfad7ca4
Adam Chehadi <39634202+APChehadi@users.noreply.github.com>
2019-11-29 18:32:49 -0700

    RTC interrupt occuring every minute and toggling LED. NEED to perform crystal offset error calibration. ~10secs off after 1.5hrs

--- Pedometer_Watch/main.c
+++ Pedometer_Watch/main.c
@@ -1,10 +1,13 @@
 #include "msp.h"
+#include "rtc.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 
-/**
- * main.c
- */
 void main(void)
 {
 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+
+	config_rtc();
+	RTC_C_IRQHandler();
 }
---
+++ Pedometer_Watch/rtc.h
@@ -0,0 +1,17 @@
+/*
+ * rtc.h
+ *
+ *  Created on: Nov 27, 2019
+ *      Author: Adam Chehadi
+ */
+
+#ifndef RTC_H_
+#define RTC_H_
+
+
+void config_rtc(void);
+
+void RTC_C_IRQHandler(void);
+
+
+#endif /* RTC_H_ */
